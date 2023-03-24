b3aa9a079db655782ce6b095a6f66c30ed28b8e9
neeraj3161 <neerajtripathi7447@gmail.com>
2023-03-24 11:56:47 +0530

    log4net setup

--- log4netApp/Program.cs
+++ log4netApp/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace log4netApp
 {
     class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
+            Console.WriteLine("Program started");
+            Log.Info("log4net configured");
+            Console.ReadLine();
         }
     }
 }
